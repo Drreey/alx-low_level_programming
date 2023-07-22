69794116b3fb64aa1c34bca6866e1d20092b91f3
irene <erzuahirene@gmail.com>
2023-07-22 15:07:40 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most of the digits (0 through 9) using _putchar
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+
+	_putchar('\n');
+}
+
