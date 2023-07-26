db9de8648a97f7d9e2d5a73064e5306ad8dc8859
irene <erzuahirene@gmail.com>
2023-07-26 15:57:17 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The input string.
+ *
+ * Return: None (void).
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The input string.
+ *
+ * Return: None (void).
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
