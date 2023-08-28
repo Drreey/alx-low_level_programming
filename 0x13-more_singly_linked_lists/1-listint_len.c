88dc3684128a98038516f3c9c9bee25923ff8f05
Drreey <erzuahirene@gmail.com>
2023-08-28 13:06:44 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/*
+ * size_t - number of nodes
+ * listint_len- function to print length or number of elements
+ * h - pointer pointing to the nodes
+ *
+ * Return ; number of element in listint
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t lenght = 0;
+
+	while (h != NULL)
+	{
+		length++
+		h = h->next;
+	}
+	return (lenght);
+}
+
