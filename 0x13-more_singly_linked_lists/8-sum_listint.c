38b9ce9b1dd836efe7337ee95aa12f61139655dd
Drreey <erzuahirene@gmail.com>
2023-08-28 17:12:26 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - calculates the sum of data in a listint_t list
  * @head: first node in the linked list
  *
- * return: the computed sum
+ * Return: the computed sum
  */
 
 int sum_listint(listint_t *head)
