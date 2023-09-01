73432517a04566db37f1f290b4f1734b24d16d16
Drreey <erzuahirene@gmail.com>
2023-09-01 15:02:55 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,26 +4,24 @@
  * binary_to_uint - converts  binary didgit to unsigned integer
  * @b : string containing binary number to be changed
  *
- * Return: the converted number, or 
- * 0 if there is one or more chars in the string b that is not 0 or 1
- * b is NULL
- *
+ * Return: the converted number, or 0 if there is one or more chars in the string 
  */
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (int i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return 0;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
 
-		result = (result <<1) | (b[i] - '0');
+		result = (result << 1) | (b[i] - '0');
 	}
 
-	return result;
+	return (result);
 }
