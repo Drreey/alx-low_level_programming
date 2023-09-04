a499c6b53e4e53b5cecdf643916841993b11660d
Drreey <erzuahirene@gmail.com>
2023-09-04 16:39:18 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,8 +26,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	write_result = write(file_descriptor, text_content, strlen(text_content));
-	if (write_result == -1)
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
 		{
 			close(file_descriptor);
 			return (-1);
