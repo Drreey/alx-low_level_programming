d499ee2a91d5e4fc5ca5789c1f541e6960efa6bf
Drreey <erzuahirene@gmail.com>
2023-09-04 16:43:10 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,11 +27,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	write_result = write(file_descriptor, text_content, strlen(text_content));
-	if (write_result == -1)
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
 		{
 			close(file_descriptor);
-			return (-1);
+				return (-1);
 		}
 	}
 
